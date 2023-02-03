bf23449f3e3743a12349b9fff95fd169c6386959
BobSarfo-at-670031179633 <robertsarfo@gmail.com>
2023-02-03 20:32:03 +0000

    Add Validation From Form Tag

---
+++ Pages/Form.razor.cs
@@ -0,0 +1,28 @@
+﻿using hq_blazor_code_challenge.DAL.Entities;
+using hq_blazor_code_challenge.Services;
+using System.Threading.Tasks;
+
+namespace hq_blazor_code_challenge.Pages
+{
+    public partial class Form
+    {
+        private readonly ICreditCardRepository _creditCardRepository;
+
+        public Form(ICreditCardRepository creditCardRepository)
+        {            
+            _creditCardRepository = creditCardRepository;
+        }
+        public CreditCard CreditCard { get; set; } = new CreditCard();
+
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+
+        public async void HandleValidSubmit()
+        {
+            await _creditCardRepository.AddNewCreditCard(CreditCard);
+        }
+    }
+}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,9 @@
+﻿using hq_blazor_code_challenge.DAL.Entities;
+
+namespace hq_blazor_code_challenge.Pages
+{
+    public partial class Index
+    {
+
+    }
+}
